57201025d1c38ee73021dafffe226d7b93fcea77
prashantsapk <prashantsapkota148@gmail.com>
2026-03-20 10:41:38 +0545

    Basic changes and created superuseer

---
+++ Ecommerce/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import redirect
+
+
+# Create your views here.
+
+def redirecttohomepage(request):
+
+    return redirect('homepage')
+   
---
+++ Home/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+
+class jeanmodel(models.Model):
+    pass
+
+class tshirtmodel(models.Model):
+    pass
+
+class sweatermodel(models.Model):
+    pass
+
+class bagpackmodel(models.Model):
+    pass
+
